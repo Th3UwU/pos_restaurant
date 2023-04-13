8b25f44dcf0f7e62e1f3a87630fb3992b1ea7e04
Th3UwU <angel.storres@alumnos.udg.mx>
2023-04-13 11:00:05 -0600

    generic am

--- src/gui/am.ts
+++ src/gui/am.ts
@@ -24,18 +24,17 @@ async function MAIN(): Promise<void> {
 	let tableInfo = await main.querySQL(`SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'public' AND TABLE_NAME = '${main.aux.column}';`);
 	
 	// Create column info array
-	for (const c of tableInfo.rows) {
+	for (const c of tableInfo.rows)
 		column.push({name: c.column_name, type: c.data_type});
-	}
 
 	// Form container
 	let form = document.getElementById('form') as HTMLDivElement;
-	
+
 	// Create HTML inputs, Ignore the first one (ID)
 	for (let i = 1; i < column.length; i++) {
 
 		let inputContainer = document.createElement('div') as HTMLDivElement;
-		let id: string = `id_${column[i].name}`;
+		let id: string = `input_${column[i].name}`;
 
 		// Input label
 		let inputLabel = document.createElement('label') as HTMLLabelElement;
@@ -91,36 +90,102 @@ async function MAIN(): Promise<void> {
 			break;
 
 		};
-		
 	}
 
-	// Accept button
-	let button_accept = document.getElementById('button_accept') as HTMLButtonElement;
-	button_accept.addEventListener('click', async (): Promise<void> => {
+	if (main.aux.action == 'a')
+	{		
+		// Accept button
+		let button_accept = document.getElementById('button_accept') as HTMLButtonElement;
+		button_accept.addEventListener('click', async (): Promise<void> => {
 
-		let query: string = `INSERT INTO ${main.aux.column} VALUES((SELECT MAX(id_${main.aux.column}) FROM ${main.aux.column}) + 1`;
+			let query: string = `INSERT INTO ${main.aux.column} VALUES((SELECT MAX(id_${main.aux.column}) FROM ${main.aux.column}) + 1`;
+			for (let i = 1; i < column.length; i++) {
+				
+				let id: string = `input_${column[i].name}`;
+
+				switch (column[i].type) {
+
+					case 'integer': case 'bigint': case 'text':
+						query += `, '${(document.getElementById(id) as HTMLInputElement).value}'`;
+						break;
+
+					case 'boolean':
+						query += `, '${(document.getElementById(id) as HTMLInputElement).checked}'`;
+						break;
+				};
+			}
+
+			query += `);`;
+			console.log(query);
+
+			try {
+				await main.querySQL(query);
+			}
+			catch (error: any)
+			{
+				console.log(error);
+				dialog.showMessageBoxSync(getCurrentWindow(), {title: "Error", message: error.message, type: "error"});
+			}
+		});
+	}
+	else if (main.aux.action == 'm')
+	{
+		// Get current entry info
+		let entry = (await main.querySQL(`SELECT * FROM ${main.aux.column} WHERE id_${main.aux.column} = ${main.aux.id};`)).rows[0];
+
+		// Fill inputs
 		for (let i = 1; i < column.length; i++) {
-			
-			let id: string = `id_${column[i].name}`;
+				
+			let id: string = `input_${column[i].name}`;
 
 			switch (column[i].type) {
 
 				case 'integer': case 'bigint': case 'text':
-					query += `, '${(document.getElementById(id) as HTMLInputElement).value}'`;
+					(document.getElementById(id) as HTMLInputElement).value = entry[column[i].name];
 					break;
 
 				case 'boolean':
-					query += `, '${(document.getElementById(id) as HTMLInputElement).checked}'`;
+					(document.getElementById(id) as HTMLInputElement).checked = entry[column[i].name];
 					break;
+			};
+		}
+		
+		// Accept button
+		let button_accept = document.getElementById('button_accept') as HTMLButtonElement;
+		button_accept.addEventListener('click', async (): Promise<void> => {
+
+			let query: string = `UPDATE ${main.aux.column} SET `;
+			for (let i = 1; i < column.length; i++) {
+				
+				let id: string = `input_${column[i].name}`;
+
+				switch (column[i].type) {
+
+					case 'integer': case 'bigint': case 'text':
+						query += `${column[i].name} = '${(document.getElementById(id) as HTMLInputElement).value}', `;
+						break;
+
+					case 'boolean':
+						query += `${column[i].name} = '${(document.getElementById(id) as HTMLInputElement).checked}', `;
+						break;
+				};
 			}
 
-		}
+			query = query.slice(0, -2);
+			query += ` WHERE id_${main.aux.column} = ${main.aux.id};`;
+			console.log(query);
 
-		query += `);`;
-		console.log(query);
+			try {
+				await main.querySQL(query);
+			}
+			catch (error: any)
+			{
+				console.log(error);
+				dialog.showMessageBoxSync(getCurrentWindow(), {title: "Error", message: error.message, type: "error"});
+			}
+		});
 
-
-	});
+	}
 
 }
 MAIN();
